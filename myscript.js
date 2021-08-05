5fa47276f02dab2c8ef43a408bb42d75af3baadb
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-05 19:29:01 +0200

    finalized excercise

--- myscript.js
+++ myscript.js
@@ -1,20 +1,43 @@
-//Chiedere a utente il numero di km che vuole percorrere e l'età del passeggero
 
-let kmDesiderati = prompt("Quanti km desidera percorrere?");
-let etàPasseggero = prompt("Quanti anni ha?");
+let kmDesiderati = parseInt(prompt("Quanti km desidera percorrere?"));
 
-//messo per vedere su schermo cosa succede
+//tentativo per ripetere alert finchè non viene inserito numero corretto
+/*do {
+       alert ("Si prega di digitare un numero corretto");
+       } while (Number.isNaN(kmDesiderati));
+} else {
+    kmDesiderati
+}
+*/
+
+let etàPasseggero = parseInt(prompt("Quanti anni ha?"));
+
+//messo per vedere su html cosa succede
 document.getElementById("km-desiderati").innerHTML = kmDesiderati;
 document.getElementById("età-passeggero").innerHTML = etàPasseggero;
 
 
-let prezzoBigliettoStandard = (kmDesiderati * 0.21);
+let prezzoBiglietto = (kmDesiderati * 0.21);
 
 if (etàPasseggero <18) {
-    document.getElementById("prezzo-biglietto-20").innerHTML = Math.round((prezzoBigliettoStandard - (prezzoBigliettoStandard * 0.2) + Number.EPSILON) * 100) / 100;
+
+    console.log(Math.round((prezzoBiglietto - (prezzoBiglietto * 0.2) + Number.EPSILON) * 100) / 100);
+
+    document.getElementById("prezzo-biglietto").innerHTML = Math.round((prezzoBiglietto - (prezzoBiglietto * 0.2) + Number.EPSILON) * 100) / 100;
+
 } else if (etàPasseggero >65) {
-    document.getElementById("prezzo-biglietto-40").innerHTML = Math.round((prezzoBigliettoStandard - (prezzoBigliettoStandard * 0.4) + Number.EPSILON) * 100) / 100;
+
+    console.log(Math.round((prezzoBiglietto - (prezzoBiglietto * 0.4) + Number.EPSILON) * 100) / 100);
+
+    document.getElementById("prezzo-biglietto").innerHTML = Math.round((prezzoBiglietto - (prezzoBiglietto * 0.4) + Number.EPSILON) * 100) / 100;
+
 } else {
-    document.getElementById("prezzo-biglietto-standard").innerHTML = Math.round((prezzoBigliettoStandard + Number.EPSILON) * 100) / 100;
+
+    console.log(Math.round((prezzoBiglietto + Number.EPSILON) * 100) / 100);
+
+    document.getElementById("prezzo-biglietto").innerHTML = Math.round((prezzoBiglietto + Number.EPSILON) * 100) / 100;
 }
+
+
+
 
